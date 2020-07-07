5324c79cf480de34c881803cab75a022164ee73d
PEYRIDIEUX Augustin (SAAGIE) <augustin.peyridieux@saagie.com>
2020-07-07 11:48:38 +0200

    add main to classes

--- src/main/scala/io/saagie/spark/template_test.scala
+++ src/main/scala/io/saagie/spark/template_test.scala
@@ -4,7 +4,9 @@ import com.typesafe.config.{Config, ConfigFactory}
 import org.slf4j.{Logger, LoggerFactory}
 
 object template_test {
-  val config: Config = ConfigFactory.load()
-  val logger: Logger = LoggerFactory.getLogger(getClass)
-  logger.info("hello world 1")
+  def main(args: Array[String]) {
+    val config: Config = ConfigFactory.load()
+    val logger: Logger = LoggerFactory.getLogger(getClass)
+    logger.info("hello world 1")
+  }
 }
--- src/main/scala/io/saagie/spark/template_test_2.scala
+++ src/main/scala/io/saagie/spark/template_test_2.scala
@@ -4,7 +4,9 @@ import com.typesafe.config.{Config, ConfigFactory}
 import org.slf4j.{Logger, LoggerFactory}
 
 object template_test_2 {
-  val config: Config = ConfigFactory.load()
-  val logger: Logger = LoggerFactory.getLogger(getClass)
-  logger.info("hello world 2")
+  def main(args: Array[String]) {
+    val config: Config = ConfigFactory.load()
+    val logger: Logger = LoggerFactory.getLogger(getClass)
+    logger.info("hello world 2")
+  }
 }
